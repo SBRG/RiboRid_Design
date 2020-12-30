a6e68158310043122ed1c612ab8a37df0fc3486c
sapoudel <sapoudel@ucsd.edu>
2020-12-30 00:23:03 -0800

    Added tests for rRNA object.

--- riborid/tests/tests.py
+++ riborid/tests/tests.py
@@ -10,17 +10,21 @@ from riborid import design_oligos, experiment, rRNA
 def test_rrna_wgbk():
     "test if rrna object is created properly with gbk input"
 
-    rrna_test = rRNA.RRNA('ex', '23S', 'tests/example_data/Saureus_TCH1516.gb', 'genbank', outdir='test_out')
+    rrna_test = rRNA.RRNA('ex', '23S', 'tests/example_data/Saureus_TCH1516.gb',
+                          'genbank', outdir='test_out')
     rrna_checksum(rrna_test)
 
 def test_rrna_wfa():
     " test if rrna object is created properly with rrna fasta input "
-    rrna_test = rRNA.RRNA('ex', '23S', 'tests/example_data/Saureus_TCH1516_23S.fa', 'fasta', outdir='test_out')
+    rrna_test = rRNA.RRNA('ex', '23S', 'tests/example_data/Saureus_TCH1516_23S.fa',
+                          'fasta', outdir='test_out')
     rrna_checksum(rrna_test)
 
 def rrna_checksum(rrna_test):
-    assert hashlib.md5(open(rrna_test.rrna_fa, 'rb').read()).hexdigest() ==
-    assert hashlib.md5(open(rrna_test.consensus, 'rb').read()).hexdigest() ==
+    consesus_hex = 'b3c97db85f8210009be98751bf076f99'
+    rrna_hex = '6e5c23c08f91ceab5967a22fcd34feb9'
+    assert hashlib.md5(open(rrna_test.rrna_fa, 'rb').read()).hexdigest() == rrna_hex
+    assert hashlib.md5(open(rrna_test.consensus, 'rb').read()).hexdigest() == consesus_hex
 
 
 # TODO: Need to write tests for rRNA.py
